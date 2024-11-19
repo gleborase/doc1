151bfdce49fbe18ff3f12a20286108130245e704
gleborase <krasnopolsci@gmail.com>
2024-11-19 22:03:16 +0200

    add doc

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        ////Task1
+        //static void Main(string[] args)
+        //{
+        //    // Получаем имя пользователя
+        //    Console.Write("Как тебя зовут? ");
+        //    string userName = Console.ReadLine();
+        //    // Вызываем методы для приветствия  и прощания
+        //    SayHello(userName);
+        //    SayBuy(userName);
+        //    Console.Read();
+        //}
+        //// Метод для приветствия
+        //static void SayHello(string name)
+        //{
+        //    Console.WriteLine($"Привет! {name}");
+        //}
+        //// Метод для прощания
+        //static void SayBuy(string name)
+        //{
+        //    Console.WriteLine($"Пока! {name}");
+        //}
+
+        ////Task2
+        //static void Main(string[] args)
+        //{
+        //    Console.Write("Первое слогаемое ");
+        //    int x = int.Parse(Console.ReadLine());
+        //    Console.Write("Второе слогаемое ");
+        //    int y = int.Parse(Console.ReadLine());
+        //    Console.Write($"Сумма: {Sum(x, y)}");
+        //    Console.Read();
+        //}
+        //public static int Sum(int x, int y)
+        //{
+        //    return x + y;
+        //}
+
+        ////Task3
+        //static void Main(string[] args)
+        //{
+        //    Console.Write("Введите a:  ");
+        //    double a = double.Parse(Console.ReadLine());
+        //    double b = 0;
+        //    Console.Write($"b = {Result(b, a)}");
+        //    Console.Read();
+        //}
+        //public static double Result(double b, double a)
+        //{
+        //    return 12 * Math.Pow(a, 2) + 7 * a - 6;
+        //}
+
+        ////Task4
+        //static void Main(string[] args)
+        //{
+        //    Console.Write("x= ");
+        //    int x = int.Parse(Console.ReadLine());
+        //    Console.Write("y= ");
+        //    int y = int.Parse(Console.ReadLine());
+        //    Console.WriteLine($"Sum={Sum(x, y)}\tRaz={Raz(x, y)}\tProiz={Proiz(x, y)}\tDel={Del(x, y)}");
+        //    Console.Read();
+        //}
+        //static int Sum(int x, int y)
+        //{
+        //    return x + y;
+        //}
+        //static int Raz(int x, int y)
+        //{
+        //    return x + (-y);
+        //}
+        //static int Proiz(int x, int y)
+        //{
+        //    return x * y;
+        //}
+        //static double Del(int x, int y)
+        //{
+        //    return x / y;
+        //}
+    }
+}
+    
