348c76fab9c3c760a87af5292fbacf0435a3ff48
gleborase <krasnopolsci@gmail.com>
2024-11-26 17:31:44 +0200

    add doc

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        ////Task1
+        //public static double Func(int x)
+        //{
+        //    return (Math.Sqrt(x) + x) / 2;
+        //}
+
+        //static void Main(string[] args)
+        //{
+        //    Console.WriteLine($"y={Func(5) + Func(12) + Func(19):F2}");
+        //    Console.Read();
+        //}
+
+        ////Task2
+        //public static double Func(int x, int y)
+        //{
+        //    return (Math.Sqrt(x) + x) / (Math.Sqrt(y) + y);
+        //}
+
+        //static void Main(string[] args)
+        //{
+        //    Console.WriteLine($"y={Func(5, 7) + Func(12, 8) + Func(19, 2):F2}");
+        //    Console.Read();
+        //}
+
+        ////Task3
+        //static int Max(int x, int y)
+        //{
+        //    return (x > y) ? x : y;
+        //}
+        //static void Main(string[] args)
+        //{
+        //    Console.Write("x=");
+        //    int x = int.Parse(Console.ReadLine());
+        //    Console.Write("y=");
+        //    int y = int.Parse(Console.ReadLine());
+        //    Console.WriteLine($"Max={Max(x, y)}");
+        //    Console.Read();
+        //}
+
+        ////Task4
+        //static int Max(int x, int y)
+        //{
+        //    return (x > y) ? x : y;
+        //}
+        //static int Max1(int x, int z)
+        //{
+        //    return (x > z) ? x : z;
+        //}
+        //static int Max2(int y, int z)
+        //{
+        //    return (y > z) ? y : z;
+        //}
+        //static void Main(string[] args)
+        //{
+        //    Console.Write("x=");
+        //    int x = int.Parse(Console.ReadLine());
+        //    Console.Write("y=");
+        //    int y = int.Parse(Console.ReadLine());
+        //    Console.Write("z=");
+        //    int z = int.Parse(Console.ReadLine());
+        //    Console.WriteLine($"Max={Max(x, y)}");
+        //    Console.Read();
+        //}
+
+        ////Task5 
+        //static int Max(int x, int y)
+        //{
+        //    return (x > y) ? x : y;
+        //}
+        //static void Main(string[] args)
+        //{
+        //    int x = Input("x=");
+        //    int y = Input("y=");
+        //    Console.WriteLine($"{Max(x, y)}");
+        //    Console.Read();
+        //}
+        //public static int Input(string message)
+        //{
+        //    Console.Write(message);
+        //    return int.Parse(Console.ReadLine());
+        //}
+
+        ////Task5
+        //static int Max(int a, int b)
+        //{
+        //    return (a > b) ? a : b;
+        //}
+        //static int Max1(int c, int d)
+        //{
+        //    return (c > d) ? c : d;
+        //}
+        //static void Main(string[] args)
+        //{
+        //    Console.Write("a=");
+        //    int a = int.Parse(Console.ReadLine());
+        //    Console.Write("b=");
+        //    int b = int.Parse(Console.ReadLine());
+        //    Console.Write("c=");
+        //    int c = int.Parse(Console.ReadLine());
+        //    Console.Write("d=");
+        //    int d = int.Parse(Console.ReadLine());
+        //    Console.WriteLine($"Max={Max(a, b)+Max1(c, d)}");
+        //    Console.Read();
+        //}
+
+        
+
+        
+    }
+}
+
+
